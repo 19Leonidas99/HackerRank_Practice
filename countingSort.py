48e1972f667ce1476aeeda030de79c5842fa141c
MAYCON BALDOINO <mm3432@live.mdx.ac.uk>
2023-04-27 22:16:33 +0100

    practice

---
+++ countingSort.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    # Write your code here
+    NewList = [0] * n
+    print(NewList)
+    for i in arr:
+        NewList[i] += 1
+        
+    print(NewList)
+    
+    
+    
+
+if __name__ == '__main__':
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
