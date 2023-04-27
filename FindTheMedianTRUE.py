e1372dd2f012eb1dfcde08c3e372cf39e87b1e80
MAYCON BALDOINO <mm3432@live.mdx.ac.uk>
2023-04-27 20:15:21 +0100

    Practice

--- FindTheMedianTRUE.py
+++ FindTheMedianTRUE.py
@@ -1,35 +1,13 @@
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
-
-
-#
-# Complete the 'findMedian' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts INTEGER_ARRAY arr as parameter.
-#
-
-def findMedian(arr):
-    # Write your code here
-    
-    arr.sort()
-    return(arr[n//2])
-
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    n = int(input().strip())
-
-    arr = list(map(int, input().rstrip().split()))
-
-    result = findMedian(arr)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
+# this input list contains duplicates
+mylist = [5, 3, 5, 2, 1, 6, 6, 4] # 5 & 6 are duplicate numbers.
+# find the length of the list
+print(len(mylist), mylist)
+# create a set from the list
+myset = set(mylist)
+# find the length of the Python set variable myset
+print(len(myset), myset)
+# compare the length and print if the list contains duplicates
+if len(mylist) != len(myset):
+    print("duplicates found in the list")
+else:
+    print("No duplicates found in the list")
